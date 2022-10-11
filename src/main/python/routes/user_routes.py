7ca13cb9a3b742178deb1efd770d4042283efbe9
NicolasDelgado1228 <nsdr1228@gmail.com>
2022-10-10 22:32:21 -0500

    commit

--- src/main/python/routes/user_routes.py
+++ src/main/python/routes/user_routes.py
@@ -3,10 +3,9 @@ import json
 
 from firestore.firestore_controller import FirestoreController
 from flask import abort
+from models.user import User
 from serializers.user_serializers import GetUserByIdSerializer, SendInvitationSerializer
 from utils.decorators import use_headers
-
-from models.user import User
 
 # user_routes.py
 # Author: Nicolas Delgado

ec34a644f011515b2140907565eda4c6abeb08b7
NicolasDelgado1228 <nsdr1228@gmail.com>
2022-11-14 20:29:06 -0500

    commit

--- src/main/python/routes/activity_routes.py
+++ src/main/python/routes/activity_routes.py
@@ -1,9 +1,9 @@
 # Dependenciess
-from firestore.firestore_controller import FirestoreController
-from flask import abort
-from models.user import User
-from serializers.user_serializers import SendInvitationSerializer
-from utils.decorators import use_headers
+# from firestore.firestore_controller import FirestoreController
+# from flask import abort
+# from models.user import User
+# from serializers.user_serializers import SendInvitationSerializer
+# from utils.decorators import use_headers
 
 # user_routes.py
 # Author: Nicolas Delgado

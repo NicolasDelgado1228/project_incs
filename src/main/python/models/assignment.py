f2c7689be7087b28b3e1b2b5c9d7eb349981d12e
NicolasDelgado1228 <nsdr1228@gmail.com>
2022-11-16 15:52:00 -0500

    commit

--- src/main/python/models/assignment.py
+++ src/main/python/models/assignment.py
@@ -3,7 +3,7 @@
 # Dependencies
 from datetime import datetime
 from enum import Enum
-from typing import List, Optional
+from typing import Optional
 from uuid import uuid4
 
 from pydantic import BaseModel
